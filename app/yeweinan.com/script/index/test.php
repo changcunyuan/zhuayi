becd6b59b3d468ae918a5bdfebae1017e6404fcf
zhuayi <2179942@qq.com>
2014-03-07 15:39:38 +0800

    modify cli dirname

---
+++ app/yeweinan.com/script/index/test.php
@@ -0,0 +1,31 @@
+<?php
+class index_test extends action
+{
+
+    public function __construct()
+    {
+
+    }
+
+    function run($a = '',$b = '')
+    {
+
+      
+
+        // var_dump($a);
+        // var_dump($b);
+        
+        //print_r($this->db_index->get_test('asd'));
+        // print_r($this);
+        // $show = array('adsd'=>time());
+        // $this->display($show);
+        // $asd = serialize($this);
+        // $asd = unserialize($asd);
+        // var_dump($asd);
+
+        die("金周至,银户县;杀人放火长安县;<br/><br/> 刁蒲城,<font color=red>野渭南</font>;<br/><br/>蛮不讲理大荔县;<br/><br/> 土匪出在二华县");
+    }
+}
+
+
+
