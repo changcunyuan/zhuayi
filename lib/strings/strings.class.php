6e227cd97ac6cda819709c95757f83a40f798dec
zhuayi <2179942@qq.com>
2014-07-06 17:17:42 +0800

    add get image_list to strings

--- lib/strings/strings.class.php
+++ lib/strings/strings.class.php
@@ -370,7 +370,7 @@
     //获取内容里的图片地址
     public function get_image_list_by_strings($content)
     {
-        preg_match_all('/src=["|\'](.*?)["|\']/s', htmlspecialchars_decode($article_info['content']),$img_list);
+        preg_match_all('/src=["|\'](.*?)["|\']/s', htmlspecialchars_decode($content),$img_list);
         return $img_list[1];
     }
  }
